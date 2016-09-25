b3e6504a966a8390904763cb08143efaaf47982d
llz383455526 <383455526@qq.com>
2016-09-25 12:46:48 +0800

    test

--- src/dateType/BiddecimalDemo.java
+++ src/dateType/BiddecimalDemo.java
@@ -4,4 +4,7 @@ package dateType;
  * Created by Administrator on 2016/9/25.
  */
 public class BiddecimalDemo {
+    public static void  main(String[] argv){
+        System.out.println("hello");
+    }
 }
